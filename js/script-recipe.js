f6df5f027465a569ddc3b73ad3c352c0b29c9be9
adrianaVN <venegasadriana78@gmail.com>
2016-12-06 19:11:09 +0000

    etapa 8

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -2,5 +2,12 @@ $(document).ready(function(){
 	console.log("el documento esta listo")
 	$(".icon-menu").hide();
 	
+	$('.js-show-recipe').click(function(){
+		 $('.page').removeClass("make");
 
+	})
+	$('.js-show-make').click(function(){
+		 $('.page').addClass("make");
+
+	})
 });
