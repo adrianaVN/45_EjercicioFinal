27cf7fc72e7fd79fd5004da50b3b82780fbfbe8d
adrianaVN <venegasadriana78@gmail.com>
2016-12-06 16:43:46 +0000

    etapa 4

--- js/script-index.js
+++ js/script-index.js
@@ -2,7 +2,8 @@ $(document).ready( function(){
 
 	//La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
 	renderHighlightedRecipes(recipesArray);
-
+	$(".icon-arrow-left-alt").hide();
+	
 	function printNews(){
 		var noticias = $('#noticias');
 
@@ -22,11 +23,12 @@ function renderHighlightedRecipes(recipesArray) {
 	console.log('Recipes: ', recipesArray);
 	
 	var receta = _.where(recipesArray, {highlighted: true});
-	console.log(receta);
-	renderRecipe(receta)
+	_.each(receta, renderRecipe);
+	
+	
 
 
-}
+};
 
 /*
 * Función que se encarga de pintar UNA recetas que tenga 
@@ -36,6 +38,83 @@ function renderHighlightedRecipes(recipesArray) {
 */
 function renderRecipe(recipe) {
 	console.log('Voy a pintar la receta: ', recipe);
+
+	/*var liga = $('<a></a>');
+		liga.addClass("item-recipe");
+
+	var span1 = $('<span></span>');
+		span1.addClass("attribution");
+
+	var span2 = $('<span></span>');
+		span2.addClass("title-recipe");
+		span2.text(recipe.title);
+
+	var span3 = $('<span></span>');
+		span3.addClass("metadata-recipe");
+
+	var span4 = $('<span></span>');
+		span4.addClass("author-recipe");
+		span4.text(recipe.source.name);
+
+	var span5 = $('<span></span>');
+		span5.addClass("bookmarks-recipe");
+
+	var span6 = $('<span></span>');
+		span6.addClass("icon-bookmark");
+
+	var img =$('</img>');
+		img.attr('src',recipe.source.url);
+
+	liga.append(span1);
+	span1.append(span2);
+	span1.append(span3);
+	span3.append(span4);
+	span3.append(span5);
+	span5.append(span6);
+	liga.append(img);
+
+	console.log(liga);
+
+	$(".list-recipes").append(liga);*/
+	var a = $('<a></a>');
+	a.addClass('item-recipe');
+
+	var spanP = $('<span><span/>');
+	spanP.addClass("attribution");
+
+	var spanH1 = $('<span><span/>');
+	spanH1.addClass("title-recipe");
+	spanH1.text(recipe.title);
+
+	var spanH2 = $('<span><span/>');
+	spanH2.addClass("metadata-recipe");
+
+	var spanN1 = $('<span><span/>');
+	spanN1.addClass("author-recipe");
+	spanN1.text(recipe.source.name);
+
+	var spanN2 = $('<span><span/>');
+	spanN2.addClass("bookmarks-recipe");
+	
+	var spanBis = $('<span></span>');
+	spanBis.addClass("icon-bookmark");
+
+	var img = $('<img/>');
+	img.attr('src', recipe.source.url);
+
+
+	
+	a.append(spanP);
+	spanP.append(spanH1);
+	spanP.append(spanH2);
+	spanH2.append(spanN1);
+	spanP.append(spanN2);
+	spanN2.append(spanBis);
+	a.append(img);
+
+	console.log(a);
+	$('.list-recipes').append(a);
+
 }
 
 
@@ -56,7 +135,6 @@ function renderActivity(recipe) {
 	
 }
 $(document).ready(function(){
-	$(".icon-arrow-left-alt").hide();
 	
 
 });
