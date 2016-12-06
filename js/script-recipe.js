d48b66221f7ebfd9d81506ee174f8557e6615811
adrianaVN <venegasadriana78@gmail.com>
2016-12-06 19:25:02 +0000

    etapa 9

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -4,10 +4,15 @@ $(document).ready(function(){
 	
 	$('.js-show-recipe').click(function(){
 		 $('.page').removeClass("make");
+		 $(this).addClass("active");
+		 $('.js-show-make').removeClass("active");
 
-	})
+	});
 	$('.js-show-make').click(function(){
 		 $('.page').addClass("make");
+		 $(this).addClass("active");
+		 $('.js-show-recipe').removeClass("active");
 
-	})
+	});
+
 });
